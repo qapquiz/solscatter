565e1d90a7c43d5ef0f92fac6b7338cf0bd8de08
qapquiz <arm.armariya@gmail.com>
2022-03-21 16:52:46 +0700

    feat: add error msg

--- programs/solscatter/src/error.rs
+++ programs/solscatter/src/error.rs
@@ -4,8 +4,10 @@ use anchor_lang::prelude::*;
 pub enum SolscatterError {
     #[msg("number of rewards in drawing result must be greater than 0")]
     NumberOfRewardsMustMoreThanZero,
+    #[msg("number of rewards in drawing reulst must be less than or equal 10")]
     NumberOfRewardsMustLessOrEqualTen,
     #[msg("number of random numbers must equal to number of rewards")]
     NumberOfRandomNumbersNotMatchWithNumberOfRewards,
+    #[msg("wrong switchboard vrf account")]
     InvalidSwitchboardVrfAccount,
 }
