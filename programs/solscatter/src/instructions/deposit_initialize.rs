36b30643ee4434014d605979514503a0ce5fb35a
Ksuphawit <ksuphawit@Ksuphawits-MacBook-Pro.local>
2022-03-21 12:48:48 +0700

    initialize use deposit ref

--- programs/solscatter/src/instructions/deposit_initialize.rs
+++ programs/solscatter/src/instructions/deposit_initialize.rs
@@ -1,8 +1,10 @@
 use anchor_lang::prelude::*;
-use anchor_spl::associated_token::AssociatedToken;
-use anchor_spl::token::{TokenAccount, Mint, Token};
-use crate::state::user_deposit::UserDeposit;
-use crate::state::main_state::MainState;
+use crate::state::{
+    user_deposit::UserDeposit,
+    user_deposit_reference::UserDepositReference,
+    main_state::MainState
+};
+use crate::seed::*;
 
 #[derive(Accounts)]
 pub struct DepositInitialize<'info> {
@@ -15,38 +17,27 @@ pub struct DepositInitialize<'info> {
     )]
     pub user_deposit: Account<'info, UserDeposit>,
     #[account(
+        init,
+        payer = depositor,
+        seeds = [depositor.key().as_ref()],
+        bump,
+        space = UserDepositReference::LEN,
+    )]
+    pub user_deposit_reference: Account<'info, UserDepositReference>,
+    #[account(
         mut,
-        seeds = [b"main_state"],
+        seeds = [MAIN_STATE_SEED],
         bump,
     )]
     pub main_state: Account<'info, MainState>,
     #[account(mut)]
     pub depositor: Signer<'info>,
-
-    pub yi_underlying_mint: Box<Account<'info, Mint>>,
-    pub yi_mint: Box<Account<'info, Mint>>,
-    #[account(
-        init_if_needed,
-        payer = depositor,
-        associated_token::mint = yi_underlying_mint,
-        associated_token::authority = depositor,
-    )]
-    pub sol_ust_token_account: Account<'info, TokenAccount>,
-    #[account(
-        init_if_needed,
-        payer = depositor,
-        associated_token::mint = yi_mint,
-        associated_token::authority = depositor,
-    )]
-    pub yi_sol_ust_token_account: Account<'info, TokenAccount>,
-    pub rent: Sysvar<'info, Rent>,
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
+    pub system_program: Program<'info, System>
 }
 
 pub fn handler(ctx: Context<DepositInitialize>) -> Result<()> {
     let user_deposit = &mut ctx.accounts.user_deposit;
+    let user_deposit_reference = &mut ctx.accounts.user_deposit_reference;
     let main_state = &mut ctx.accounts.main_state;
     let depositor = &ctx.accounts.depositor;
 
@@ -54,6 +45,7 @@ pub fn handler(ctx: Context<DepositInitialize>) -> Result<()> {
     user_deposit.amount = 0;
     user_deposit.owner = depositor.key().clone();
     user_deposit.latest_deposit_timestamp = None;
+    user_deposit_reference.slot = main_state.current_slot + 1;
 
     main_state.current_slot = main_state.current_slot + 1;
     Ok(())
