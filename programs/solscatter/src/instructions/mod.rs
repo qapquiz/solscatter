6c8dec6cb20367dc350d8c541554124b5417187e
armariya <arm.armariya@gmail.com>
2022-03-30 12:49:21 +0700

    fix: quarry mine CreateMiner stack overflow

--- programs/solscatter/src/instructions/mod.rs
+++ programs/solscatter/src/instructions/mod.rs
@@ -3,7 +3,6 @@ pub mod callback_request_randomness;
 pub mod request_randomness;
 pub mod deposit_initialize;
 pub mod deposit;
-pub mod stake;
 pub mod start_drawing_phase;
 pub mod drawing;
 
@@ -12,6 +11,5 @@ pub use callback_request_randomness::*;
 pub use request_randomness::*;
 pub use deposit_initialize::*;
 pub use deposit::*;
-pub use stake::*;
 pub use start_drawing_phase::*;
 pub use drawing::*;
